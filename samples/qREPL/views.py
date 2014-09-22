c4b300b6a75c8a85848ca4fc9364f69a688c562e
buckie <wjmartino@gmail.com>
2014-09-22 00:12:46 -0400

    added in exception handling and multiline vs single support
    Signed-off-by: buckie <wjmartino@gmail.com>

--- samples/qREPL/views.py
+++ samples/qREPL/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, request, redirect, session
+from flask import render_template, request, redirect, session, flash
+from qpython.qtype import QException
 from flask_kdb import get_kdb
 from . import app
 from qpython.qcollection import QTable, QKeyedTable, QList, QDictionary, QTemporalList
@@ -26,10 +27,30 @@ def run_query():
     q_conn = get_kdb()
     qstatus = get_q_status(q_conn)
     orig_query = request.form['queryText']
-    query = orig_query.encode('ascii').replace('\n', ' ').replace('\r', '').replace('  ', ' ')
-    data = q_conn(query)
-    data = convert_qdata(data)
-    session['query'] = dict(qstatus=qstatus, orig_query=orig_query, query=query, query_result=data)
+    if 'stripNewline' in request.form:
+        stripNewline = True
+    else:
+        stripNewline = False
+    errors = False
+    if stripNewline:
+        query = orig_query.encode('ascii').replace('\n', ' ').replace('\r', '').replace('  ', ' ')
+        try:
+            data = q_conn(query)
+        except QException as e:
+            errors = True
+            flash(str(e), 'qerror')
+    else:
+        queries = [i for i in orig_query.encode('ascii').splitlines()]
+        for query in queries:
+            try:
+                data = q_conn(query)
+            except QException as e:
+                errors = True
+                flash(str(e), 'qerror')
+
+    if not errors:
+        data = convert_qdata(data)
+        session['query'] = dict(qstatus=qstatus, orig_query=orig_query, query=query, query_result=data)
     return redirect('/')
 
 
